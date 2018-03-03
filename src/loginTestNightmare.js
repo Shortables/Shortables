3c06831ab981ae287e900547afae5e31ddf9cdd9
Jack Chiu <jchk852@yahoo.com>
2018-03-03 10:50:11 -0800

    add main page to tests

--- src/loginTestNightmare.js
+++ src/loginTestNightmare.js
@@ -1,12 +1,21 @@
 var Nightmare = require('nightmare');
 
 new Nightmare({ show: true })
-.goto('http://localhost:3000/login')
+// go to main page
+.goto('http://localhost:3000/') 
+// go to login page
+.click('#nav-login')
+// enter user name
 .type('#loginName', 'JackC')
+// enter password
 .type('#loginPw', 'passw0rd')
+// click log in button
 .click('#loginBtn')
+// wait 1s
 .wait(1000)
+// take a screenshot of the result
 .screenshot("login.png")
+// end
 .end()
 .then(function() {
     console.log("Done!");
--- src/regTestNightmare.js
+++ src/regTestNightmare.js
@@ -1,30 +1,32 @@
 var Nightmare = require('nightmare');
 
 new Nightmare({ show: true })
-     // go to login page
-     .goto('http://localhost:3000/register')
-     // enter email address
-     .type('#addEmail', 'abc@123.com')
-     // enter first name
-     .type('#addFirst', 'Registration')
-     // enter last name
-     .type('#addLast','Nightmare')
-     // enter user name
-     .type('#addUserName', 'regNightmare')
-     // enter password
-     .type('#addPassword', 'passw0rd')
-     // click sign up button
-     .click('#signUp')
-     // wait 5 seconds
-     .wait(5000)
-     // take screenshot save to current dir
-     .screenshot("registered.png")
-     // end test
-     .end()
-     .then(function() {
-         console.log('Done!');
-     })
-     // catch errors
-     .catch(function(err) {
-         console.log(err);
-     });
+// go to main page
+.goto('http://localhost:3000/') 
+// go to login page
+.click('#nav-register')
+// enter email address
+.type('#addEmail', 'abc@123.com')
+// enter first name
+.type('#addFirst', 'Registration')
+// enter last name
+.type('#addLast','Nightmare')
+// enter user name
+.type('#addUserName', 'regNightmare')
+// enter password
+.type('#addPassword', 'passw0rd')
+// click sign up button
+.click('#signUp')
+// wait 5 seconds
+.wait(5000)
+// take screenshot save to current dir
+.screenshot("registered.png")
+// end test
+.end()
+.then(function() {
+    console.log('Done!');
+})
+// catch errors
+.catch(function(err) {
+    console.log(err);
+});
