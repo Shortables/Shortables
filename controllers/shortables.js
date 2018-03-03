3015edf37ecb7362f972feb37a53b676016410d3
Ruhi Kumbhani <ruhi.kumbhani@gmail.com>
2018-03-02 22:39:12 -0800

    nav bar stuff

--- controllers/shortables.js
+++ controllers/shortables.js
@@ -4,7 +4,20 @@ var db			= require("../models");
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { user : req.user });
+    posts = ['first post', 'second post', 'third post']
+    res.render('index', { user : req.user, posts : posts });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login', { user : req.user });
+});
+
+router.get('/register', (req, res) => {
+    res.render('register', { user : req.user });
+});
+
+router.get('/upload', (req, res) => {
+    res.render('upload', { user : req.user });
 });
 
 module.exports = router;
